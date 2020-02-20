6dbaccc15ce7ad76807792cbda9d751a9b44d4e8
nguyentrongdoan <nguyendoan206@gmail.com>
2020-02-20 11:01:11 +0700

    Shape in extends

---
+++ src/Shape/Rectangle.java
@@ -0,0 +1,49 @@
+package Shape;
+
+public class Rectangle extends Shape {
+    private double width = 1.0;
+    private double height = 1.0;
+
+    public Rectangle(){}
+
+    public Rectangle(String color, boolean filled, double width, double height) {
+        super(color, filled);
+        this.width = width;
+        this.height = height;
+    }
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getArea(){
+        return width * this.height;
+    }
+    public double getPerimeter(){
+        return 2 * ( width + height);
+    }
+
+    @Override
+    public String toString() {
+        return " A Rectangle with width =  " +
+                getWidth() + "and height = "
+                + getHeight() + ", which is a subclass of " + super.toString();
+    }
+}
